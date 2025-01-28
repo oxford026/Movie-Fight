a6cbfe0da8c8c7799a616fdc55144ed433d990c2
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-01-28 16:56:37 +0300

    update

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const fetchData = async () => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '406cea08', 
-            s: 'avengers'
+            i: 'tt0848228'
+            
         }
     });
 
