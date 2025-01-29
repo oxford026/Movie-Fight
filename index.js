f564e529d866f698557a63935f4a344c863377b5
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-01-29 14:23:45 +0300

    update

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const fetchData = async searchTerm => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: '406cea08',
-            s: searchTerm
+// const fetchData = async searchTerm => {
+//     const response = await axios.get('http://www.omdbapi.com/', {
+//         params: {
+//             apikey: '406cea08',
+//             s: searchTerm
 
-        }
-    });
+//         }
+//     });
 
-    console.log(response.data);
+//     console.log(response.data);
+// };
+
+// const input = document.querySelector('input');
+
+// let timeoutId;
+// const onInput = event => {
+//     if (timeoutId) {
+//         clearTimeout(timeoutId)
+//     }
+//     timeoutId = setTimeout(() => {
+//         fetchData(event.target.value);
+//     }, 2000
+//     )
+// }   
+
+
+// input.addEventListener('input', onInput);
+
+
+const greet = (name) => { 
+    console.log(`Hello, ${name}`);
 };
 
-const input = document.querySelector('input');
-
-let timeoutId;
-const onInput = event => {
-    if (timeoutId) {
-        clearTimeout(timeoutId)
-    }
-    timeoutId = setTimeout(() => {
-        fetchData(event.target.value);
-    }, 2000
-    )
-}   
-
-
-input.addEventListener('input', onInput);
-
-
+greet()
