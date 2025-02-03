91015abae732547efc812e30d889f4787c7603ef
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-02-03 18:14:02 +0300

    update

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
 const fetchData = async searchTerm => {
     const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: '406cea08',
-            s: searchTerm
-
-        }
+      params: {
+        apikey: 'd9835cc5',
+        s: searchTerm
+      }
     });
-
+  
     console.log(response.data);
-};
-
-const input = document.querySelector('input');
-
-const debounce = (func) => {
-    let timeoutId;
-    return (...args) => {
-        if (timeoutId) {
-            clearTimeout(timeoutId)
-        };
-    }
-    setTimeout(() => {
-        func.apply(null, args);
-    }, 1000);
-
-}
-
-const onInput = debounce (event => {
+  };
+  
+  const input = document.querySelector('input');
+  
+  
+  
+  const onInput = event => {
     fetchData(event.target.value);
-}
-);
-
-
-input.addEventListener('input', onInput);
-
-
+  };
+  input.addEventListener('input', debounce(onInput, 500));
+  
