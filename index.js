5190e933fa4b83fbe73156f35aeb8fed874013f3
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-02-05 01:01:30 +0300

    update

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const fetchData = async searchTerm => {
     const response = await axios.get('http://www.omdbapi.com/', {
-      params: {
-        apikey: 'd9835cc5',
-        s: searchTerm
-      }
+        params: {
+            apikey: 'd9835cc5',
+            s: searchTerm
+        }
     });
-  
+
     if (response.data.Error) {
-      return [];
+        return [];
     }
-  
+
     return response.data.Search;
-  };
-  
-  const root = document.querySelector('.autocomplete');
-  root.innerHTML = `
+};
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
     <label><b>Search For a Movie</b></label>
     <input class="input" />
     <div class="dropdown">
@@ -23,26 +23,59 @@ const fetchData = async searchTerm => {
       </div>
     </div>
   `;
-  
-  const input = document.querySelector('input');
-  const dropdown = document.querySelector('.dropdown');
-  const resultsWrapper = document.querySelector('.results');
-  
-  const onInput = async event => {
+
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+
+const onInput = async event => {
     const movies = await fetchData(event.target.value);
-  
+    if (!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
+    resultsWrapper.innerHTML = '';
     dropdown.classList.add('is-active');
     for (let movie of movies) {
-      const option = document.createElement('a');
-  
-      option.classList.add('dropdown-item');
-      option.innerHTML = `
-        <img src="${movie.Poster}" />
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+        <img src="${imgSrc}" />
         ${movie.Title}
       `;
-  
-      resultsWrapper.appendChild(option);
+
+      option.addEventListener('click', () => {
+        dropdown.classList.remove('is-active');
+        input.value = movie.Title;
+        onMovieSelect(movie);
+      });
+
+
+        resultsWrapper.appendChild(option);
+    };
+}
+input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
     }
-  };
-  input.addEventListener('input', debounce(onInput, 500));
-  
+
+
+})
+const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'd9835cc5',
+            i: movie.imdbID
+        }
+    });
+
+    console.log(response.data);
+
+   // document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
