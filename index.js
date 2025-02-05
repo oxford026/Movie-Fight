2ae9bdfee144734d4822dac2e7892b5bd2d9465f
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-02-05 14:57:19 +0300

    update

--- index.js
+++ index.js
@@ -13,52 +13,15 @@ const fetchData = async searchTerm => {
     return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-    <label><b>Search For a Movie</b></label>
-    <input class="input" />
-    <div class="dropdown">
-      <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-      </div>
-    </div>
-  `;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-
-const onInput = async event => {
-    const movies = await fetchData(event.target.value);
-    if (!movies.length) {
-        dropdown.classList.remove('is-active');
-        return;
-    }
-    resultsWrapper.innerHTML = '';
-    dropdown.classList.add('is-active');
-    for (let movie of movies) {
-        const option = document.createElement('a');
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-
-        option.classList.add('dropdown-item');
-        option.innerHTML = `
-        <img src="${imgSrc}" />
-        ${movie.Title}
-      `;
-
-      option.addEventListener('click', () => {
-        dropdown.classList.remove('is-active');
-        input.value = movie.Title;
-        onMovieSelect(movie);
-      });
-
-
-        resultsWrapper.appendChild(option);
-    };
-}
-input.addEventListener('input', debounce(onInput, 500));
+createAutoComplete({
+    root: document.querySelector('.autocomplete'),
+})
+createAutoComplete({
+    root: document.querySelector('.autocomplete-two'),
+})
+createAutoComplete({
+    root: document.querySelector('.autocomplete-three')
+})
 
 document.addEventListener('click', event => {
     if (!root.contains(event.target)) {
@@ -93,7 +56,7 @@ const movieTemplate = movieDetail => {
           <h1>${movieDetail.Title}</h1>
           <h4>${movieDetail.Genre}</h4>
           <p>${movieDetail.Plot}</p>
-          <p>Release date: ${movieDetail.Year}</p>
+          <p>Release date: ${movieDetail.Released}</p>
         </div>
       </div>
     </article>
@@ -117,7 +80,5 @@ const movieTemplate = movieDetail => {
       <p class="title">${movieDetail.imdbVotes}</p>
       <p class="subtitle">IMDB Votes</p>
     </article>
-    
- 
-  `;  
+  `;
 }
